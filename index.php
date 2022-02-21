7faf01422903638fd6e76a7285c00b20a3b27beb
paulinskiz <p.j.paulinskiz@gmail.com>
2022-02-21 21:43:17 +0200

    medilab_design

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medilab</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:wght@200;300;400&family=Changa+One&family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body>
+    <!-- Main box -->
+    <div class="main_box">
+        <!-- top line -->
+        <div class="top_line">
+            <!-- logo -->
+            <div class="logo">
+                <a href="#" class="logo_link">
+                    <div class="medilab">Medilab</div>
+                    <div class="plus">+</div>
+                </a>
+            </div>
+            <!-- nav menu -->
+            <div class="nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Testimonial</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- intro -->
+        <div class="intro">
+            <!-- logo -->
+            <div class="logo">
+                <div class="medilab">Medilab</div>
+                <div class="plus">+</div>
+            </div>
+            <!-- heading -->
+            <span class="heading">Healthcare at your desk!!</span>
+            <!-- text -->
+            <span class="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quis aperiam quidem. Itaque consequuntur quisquam vitae quibusdam officia corrupti.</span>
+            <!-- button -->
+            <button class="button">Make an Appointment</button>
+            <!-- arrow -->
+            <a href="#down" class="arrow"><i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
+    <!-- main box end -->
+
+    <!-- content box -->
+    <div class="content" id="down">
+        <div class="content_box">
+            <div class="left">
+                <span class="heading">Our service</span>
+                <div class="line"></div>
+                <span class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus a exercitationem nulla ipsam dolorum. Dicta velit, laudantium consequuntur blanditiis omnis ex incidunt minima eius rerum eum, sed, quis eaque. Et! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, accusamus.</span>
+            </div>
+            <div class="mid">
+                <div class="top">
+                    <i class="fa-solid fa-hand-holding-medical"></i>
+                    <span class="header">24 Hour support</span>
+                    <span class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos labore, ad eligendi nisi mollitia sint. Alias dolores commodi iste! Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, enim.</span>
+                </div>
+                <div class="bottom">
+                    <i class="fa-solid fa-truck-medical"></i>
+                    <span class="header">Emergency services</span>
+                    <span class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum dolore ad modi temporibus, quod beatae perspiciatis saepe repudiandae dolores? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, voluptate.</span>
+                </div>
+            </div>
+            <div class="right">
+                <div class="top">
+                    <i class="fa-solid fa-user-doctor"></i>
+                    <span class="header">Medical Coundeling</span>
+                    <span class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos labore, ad eligendi nisi mollitia sint. Alias dolores commodi iste! Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, enim.</span>
+                </div>
+                <div class="bottom">
+                    <i class="fa-solid fa-kit-medical"></i>
+                    <span class="header">Premium Healthcare</span>
+                    <span class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum dolore ad modi temporibus, quod beatae perspiciatis saepe repudiandae dolores? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, voluptate.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
